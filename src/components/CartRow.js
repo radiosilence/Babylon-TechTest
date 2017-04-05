b2ce01a0191a7c4c2aad120aa11c966298390411
James Cleveland <jc@blit.cc>
2017-04-05 11:56:38 +0100

    This was also bugging me

--- src/components/CartRow.js
+++ src/components/CartRow.js
@@ -3,7 +3,7 @@ import { Button, Row, Col, FormControl } from 'react-bootstrap';
 
 export default ({ children, quantity, onRemove, onChangeQuantity }) =>
   <Row>
-    <Col md={6}>
+    <Col md={8}>
       {children}
     </Col>
     <Col md={2}>
